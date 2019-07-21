91724dc99334ebede9f4c43a5c366aac97c577e0
Piyush Kumar <47007766+piyush48@users.noreply.github.com>
2019-07-21 11:30:21 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,30 @@
+var number1 = 40;
+var number2 = 20;
+
+function add(number1,number2)
+{
+    var sum=number1+number2;
+    console.log(sum);
+    
+}
+
+function subtract(number1,number2)
+{
+    var subtraction=number1-number2;
+    console.log(subtraction);
+    
+}
+
+function multiplication(number1,number2)
+{
+    var multiply=number1*number2;
+    console.log(multiply);
+    
+}
+
+function division(number1,number2)
+{
+    var division=number1/number2;
+    console.log(division);
+    
+}
---
+++ multiply.js
@@ -0,0 +1,5 @@
+function multiply(){
+    document.getElementById("num1");
+    document.getElementById("num2");
+}
+document.addEventListner(click,multiply);
